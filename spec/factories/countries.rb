da7bf615f256fb1ac96836a66055a516333328c1
Roland Laurès <roland@octopuslab.fr>
2020-03-05 12:25:58 +0100

    make it to the solution

--- spec/factories/countries.rb
+++ spec/factories/countries.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :country do
+    to_create do |instance|
+      instance.attributes = Country.find_or_create_by(name: instance.name, code: instance.code).attributes
+      instance.reload
+    end
+
     name { 'Canada' }
     code { 'CA' }
   end
